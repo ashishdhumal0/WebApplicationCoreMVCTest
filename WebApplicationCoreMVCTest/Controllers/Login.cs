c01e7a62842626d9bd44dc240ac169a71718abec
SVN <SVN@DESKTOP-FQJRI29>
2024-03-19 17:40:20 +0530

    Add project files.

---
+++ WebApplicationCoreMVCTest/Controllers/Login.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationCoreMVCTest.Controllers
+{
+    public class Login : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login_Index()
+        {
+            return View();
+        }
+    }
+}
